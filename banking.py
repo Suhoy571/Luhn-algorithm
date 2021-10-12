edd9b0c1c4902591b41c4e435d2dee7d97b88519
Suhovienko Ilia <isuhovienko@gmail.com>
2021-10-12 16:12:00 +0300

    End

--- banking.py
+++ banking.py
@@ -13,7 +13,7 @@ class Card:
     def create_account(self):
         while True:
             self.card_number = int("400000" + str(''.join([str(random.randint(0, 9)) for _ in range(10)])))
-            if self.luhn_algorithm():
+            if self.luhn_algorithm(self.card_number):
                 break
         self.card_pin = ''.join([str(random.randint(0, 9)) for _ in range(4)])
         self.balance = 0
@@ -33,11 +33,14 @@ class Card:
     def login(self, number, pin):
         self.cur.execute(f"SELECT * FROM card WHERE pin = {pin} and number = {number}")
         account = self.cur.fetchall()
-        if account[0][1] == str(number) and account[0][2] == str(pin):
-            print('\nYou have successfully logged in!\n')
-            return True
-        else:
-            print("\nWrong card number or PIN!\n")
+        try:
+            if account[0][1] == str(number) and account[0][2] == str(pin):
+                print('\nYou have successfully logged in!\n')
+                return True
+            else:
+                print("\nWrong card number or PIN!\n")
+                return False
+        except:
             return False
 
     def get_customer_balance(self, number, pin):
@@ -57,6 +60,16 @@ class Card:
         self.cur.execute(f"SELECT balance FROM card WHERE number = {card_number};")
         current_balance = self.cur.fetchall()
         self.cur.execute(f"SELECT balance FROM card WHERE number = {card_transfer};")
+        transfer_to_balance = self.cur.fetchall()
+        if int(current_balance[0][0]) < int(money_to_transfer):
+            return "Not enough money!\n"
+        else:
+            balance = int(transfer_to_balance[0][0]) + (int(current_balance[0][0]) - int(money_to_transfer))
+            self.cur.execute(f"UPDATE card SET balance = {balance} WHERE number = {card_transfer};")
+            self.cur.execute(
+                f"UPDATE card SET balance = {int(current_balance[0][0]) - int(money_to_transfer)} WHERE number = {card_number};")
+            self.conn.commit()
+            return "Success!\n"
 
     def close_account(self, number, pin):
         self.cur.execute(f"DELETE FROM card WHERE pin = {pin} and number = {number};")
@@ -77,9 +90,9 @@ class Card:
             print(i)
         print()
 
-    def luhn_algorithm(self):
+    def luhn_algorithm(self, card_number):
         multi_card_number = []
-        for i, value in enumerate(str(self.card_number)[:-1]):
+        for i, value in enumerate(str(card_number)[:-1]):
             if i % 2 == 0:
                 value = int(value) * 2
                 if int(value) > 9:
@@ -89,7 +102,7 @@ class Card:
                     multi_card_number.append(value)
             else:
                 multi_card_number.append(value)
-        if (sum([int(k) for k in multi_card_number]) + int(str(self.card_number)[-1])) % 10 == 0:
+        if (sum([int(k) for k in multi_card_number]) + int(str(card_number)[-1])) % 10 == 0:
             return True
         else:
             return False
@@ -118,11 +131,18 @@ while True:
                 if menu == "3":
                     print('\nTransfer')
                     card_to_transfer = input('Enter card number: \n')
-                    if customer.check_if_card_exist(card_to_transfer):
-                        customer.do_transfer(card_number, card_to_transfer,
-                                             input('Enter how much money you want to transfer: \n'))
+                    if card_to_transfer == card_number:
+                        print("You can't transfer money to the same account!\n")
+                        continue
+                    if customer.luhn_algorithm(card_to_transfer):
+                        if customer.check_if_card_exist(card_to_transfer):
+                            print(customer.do_transfer(card_number, card_to_transfer,
+                                                       input('Enter how much money you want to transfer: \n')))
+                        else:
+                            print("Such a card does not exist.\n")
                     else:
-                        print("Such a card does not exist.\n")
+                        print("Probably you made a mistake in the card number. Please try again!\n")
+
                 if menu == "4":
                     customer.close_account(number=card_number, pin=pin)
                     print("The account has been closed!\n")
@@ -132,6 +152,8 @@ while True:
                     break
                 if menu == "0":
                     break
+        else:
+            print("Wrong card number or PIN!\n")
     if menu == "8":
         customer.get_all_data()
     if menu == "0":
